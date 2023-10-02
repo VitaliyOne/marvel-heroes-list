e5453f6f79e0f017b4214276baaed49216db3f57
VitaliyOne <vitaliy.one1@yandex.ru>
2023-10-02 22:17:29 +0300

    Connecting the marvel API, code review

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import MyButton from "./components/UI/button/MyButton";
 import Header from "./components/Header";
 import MyInput from "./components/UI/input/MyInput";
@@ -7,6 +8,22 @@ import HeroCard from "./components/HeroCard";
 import Footer from "./components/Footer";
 
 function App() {
+  const offset = Math.round(Math.random() * 100);
+  const url = `https://gateway.marvel.com/v1/public/characters?limit=10&offset=${offset}&ts=${
+    import.meta.env.VITE_TS
+  }&apikey=${import.meta.env.VITE_PUBLIC_API_KEY}&hash=${
+    import.meta.env.VITE_HASH
+  }`;
+  const [heroes, setHeroes] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => setHeroes(data.data.results));
+  }, []);
+  console.log(heroes);
   return (
     <>
       <div className="App">
@@ -15,7 +32,7 @@ function App() {
         <div style={{ borderRadius: "10px", backgroundColor: "white" }}>
           <div className="toolbar">
             <MyInput className="inputNameHero" placeholder="Name hero" />
-            <MyButton />
+            <MyButton></MyButton>
           </div>
           <div className="toolbar">
             <MySelect />
@@ -23,12 +40,9 @@ function App() {
             <MySelect />
           </div>
           <div className="listHeroes">
-            <HeroCard />
-            <HeroCard />
-            <HeroCard />
-            <HeroCard />
-            <HeroCard />
-            <HeroCard />
+            {heroes.map((hero) => (
+              <HeroCard hero={hero} key={hero.id} />
+            ))}
           </div>
         </div>
         <div style={{ marginTop: "20px" }}></div>
--- src/components/HeroCard.tsx
+++ src/components/HeroCard.tsx
@@ -1,13 +1,18 @@
-const HeroCard = () => {
+const HeroCard = (props) => {
+  const img = `${props.hero.thumbnail.path}.${props.hero.thumbnail.extension}`;
   return (
     <div className="HeroCard">
       <img
-        style={{ width: "100%", objectFit: "contain", borderRadius: "10px" }}
-        src="https://bobbyhadz.com/images/blog/react-prevent-multiple-button-clicks/thumbnail.webp"
-        alt="car"
+        style={{
+          width: "250px",
+          objectFit: "contain",
+          borderRadius: "10px",
+        }}
+        src={img}
+        alt="Hero image"
       />
-      <h3>Hero name</h3>
-      <p>Hero info</p>
+      <h3>{props.hero.name}</h3>
+      <p>{props.hero.description}</p>
     </div>
   );
 };
